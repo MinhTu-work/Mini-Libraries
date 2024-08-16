49534a72aa82cf515d20970b660bd8f5b4129190
MinhTu-work <tunm.work@gmail.com>
2024-08-16 12:24:55 +0700

    Uploaded

---
+++ AbstractService + AbstractMapper/src/main/java/com/example/Client/repository/TourRepository.java
@@ -0,0 +1,11 @@
+package com.example.Client.repository;
+import com.example.Client.entity.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+    @Override
+    <S extends Tour> S save(S entity);
+    findAll
+}
